c7b87584704b84d41b8c284eff3ed70dc5a4aede
Noor Abid <your.email@example.com>
2025-07-25 15:18:08 +1000

    Hompage first hero section setup and prelim styling

---
+++ frontend/src/components/RotatingText.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+const RotatingText = () => {
+  const interests = [
+    "enjoy solving problems through code ⚙️",
+    "am one of those nerds that play dungeons & dragons 🧙‍♂️",
+    "am tall but terrible at basketball ⛹️",
+    "love motorsport 🏎️",
+    "got stuck in tutorial hell 🤦‍♂️",
+    "am (or at least try to be) a fullstack developer 👨‍💻",
+  ];
+
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prev) => (prev + 1) % interests.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  });
+
+  return <span>I {interests[index]}</span>;
+};
+
+export default RotatingText;
